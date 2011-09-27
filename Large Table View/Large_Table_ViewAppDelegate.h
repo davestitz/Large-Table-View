4f44cbb7212f43f5876070d06651cffe6bb694fd
David Stitzinger <davestitz@gmail.com>
2011-09-27 08:48:26 -0400

    Initial Commit

---
+++ Large Table View/Large_Table_ViewAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  Large_Table_ViewAppDelegate.h
+//  Large Table View
+//
+//  Created by David Stitzinger on 9/27/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Large_Table_ViewAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
---
+++ Large Table View/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Large Table View
+//
+//  Created by David Stitzinger on 9/27/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+
+
+@end
